ff031f0c5df22a1bd21d8b7c1a985ebafb3f72e1
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2023-12-04 10:38:50 -0500

    Fix the trim implementation

--- include/cpp_utils/strings.h
+++ include/cpp_utils/strings.h
@@ -12,13 +12,9 @@ namespace cpp_utils
 CPP_UTILS_EXPORT [[nodiscard]] constexpr std::string_view trim(const std::string_view view) noexcept
 {
     constexpr std::string_view SPACE_CHARACTERS = " \n\t\r";
-    constexpr auto SPACE_CHARACTERS_BEGIN = SPACE_CHARACTERS.begin();
-    constexpr auto SPACE_CHARACTERS_END = SPACE_CHARACTERS.end();
-    constexpr auto SPACE_CHARACTERS_RBEGIN = SPACE_CHARACTERS.crbegin();
-    // constexpr auto SPACE_CHARACTERS_REND = SPACE_CHARACTERS.crend();
     const auto isNotACharacterASpaceCharacter = [&](const char& c) noexcept
     {
-        return std::ranges::find(SPACE_CHARACTERS, c) != SPACE_CHARACTERS_END;
+        return std::ranges::find(SPACE_CHARACTERS, c) == std::ranges::end(SPACE_CHARACTERS);
     };
 
     if(view.size() == 0)
@@ -28,10 +24,12 @@ CPP_UTILS_EXPORT [[nodiscard]] constexpr std::string_view trim(const std::string
 
     std::string_view returnValue = view;
     auto&& firstNonSpaceCharacter = std::ranges::find_if(returnValue, isNotACharacterASpaceCharacter);
-    returnValue.remove_prefix(std::ranges::distance(SPACE_CHARACTERS_BEGIN, firstNonSpaceCharacter));
+    const auto distance = std::ranges::distance(std::ranges::cbegin(returnValue), firstNonSpaceCharacter);
+    returnValue.remove_prefix(distance);
 
     auto&& firstReverseNonSpaceCharacter = std::ranges::find_if(std::crbegin(returnValue), std::crend(returnValue), isNotACharacterASpaceCharacter);
-    returnValue.remove_suffix(std::ranges::distance(SPACE_CHARACTERS_RBEGIN, firstReverseNonSpaceCharacter));
+    const auto reverseDistance = std::ranges::distance(std::crbegin(returnValue), firstReverseNonSpaceCharacter);
+    returnValue.remove_suffix(reverseDistance);
 
     return returnValue;
 }
